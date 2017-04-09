82362296add4ff1558f5cc25a9bcbe4a46e66614
Rafiableh <saiyuriye.rafia@gmail.com>
2017-04-09 15:41:33 +0300

    ITP-Round03
    Fishes in an aquarium require a certain water pH level, so aquarium hobbyists must regularly measure the water’s pH. The value range must be suitable for fishes and cannot vary too much. In this task, we shall create a program that defines the water’s suitability for zebra fishes.

---
+++ Project01.py
@@ -0,0 +1,31 @@
+#Introduction to Programming
+#Project01
+#Student Name: Rafia Bushra
+#Student ID: 268449
+
+def main():
+    prev_result = 0.0
+    result = 0.0
+    count = int(input("Enter the number of the measurements:"))
+    flag = 0
+    if count <= 0:
+        print("Error: the number must be expressed as a positive integer.")
+    else:
+        for i in range(1,count+1):
+            next_result = float(input("Enter the measurement result "+str(i)
+                                      + ": "))
+            if next_result > 8 or next_result < 6:
+                print("The conditions are not suitable for zebra fishes.")
+                flag = 1
+                break
+            elif i > 1 and abs(next_result - prev_result) > 1.0:
+                print("The conditions are not suitable for zebra fishes.")
+                flag = 1
+                break
+            else:
+                result += next_result
+                prev_result = next_result
+        if flag == 0:
+            print("Conditions are suitable for zebra fishes. The average pH is"
+                  " ", format(result/count, '.2f'), ".", sep="")
+main()
