f815568460a7cf927c3efc2b6294a0e6f152a865
Rafiableh <saiyuriye.rafia@gmail.com>
2017-04-09 15:45:19 +0300

    ITP-Round05
    Let's assume that at some less inhabited area, buses leave for Tampere according to the following schedule:
    6.30
    10.15
    14.15
    16.20
    17.20
    20.00
    Design and implement a program that asks the user what time it is and prints the times for the next three buses, based of the entered time.

---
+++ R05T09.py
@@ -0,0 +1,18 @@
+def main():
+    list = [630,1015,1415,1620,1720,2000]
+    time = int(input("Enter the time (as an integer): "))
+    i = 0
+    count = 0
+    print("The next buses leave:")
+    while count != 3:
+        if i >= len(list) and count < 3:
+            print(list[i- len(list)])
+            count += 1
+            i += 1
+            continue
+        if time <= list[i]:
+            print(list[i])
+            count += 1
+        i += 1
+
+main()
